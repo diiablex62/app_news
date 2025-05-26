355210735e96a10ca4bba8a0cd3cb04ad413bdf8
Aleexj <jalias59@outlook.fr>
2025-05-26 12:02:06 +0200

    Modification de la logique de démarrage du serveur pour utiliser le port spécifié dans les variables d'environnement ou le port par défaut 3000, améliorant ainsi la flexibilité de la configuration du serveur.

--- back/index.js
+++ back/index.js
@@ -45,7 +45,7 @@ app.get(/(.*)/, (req, res) => {
 
 mongoose.connect(process.env.MONGO_URL).then(() => {
   console.log("✅ MongoDB connected");
-  app.listen(process.env.PORT, () =>
-    console.log("🚀 Server on http://localhost:3000")
+  app.listen(process.env.PORT || 3000, () =>
+    console.log(`🚀 Server running on port ${process.env.PORT || 3000}`)
   );
 });
