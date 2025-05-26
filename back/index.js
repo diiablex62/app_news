75df285d18e8230ef82b1d5e8214b73a2629dd74
Aleexj <jalias59@outlook.fr>
2025-05-26 09:56:24 +0200

    Ajout de la gestion des fichiers statiques pour l'environnement de production et amélioration de la structure du serveur Express.

--- back/index.js
+++ back/index.js
@@ -1,18 +1,27 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
-
 app.use(express.json());
 
 const postRoutes = require("./routes/posts");
-
 app.use("/api/posts", postRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  const __DIRNAME = path.resolve();
+  app.use(express.static(path.join(__DIRNAME, "front/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__DIRNAME, "front", "dist", "index.html"));
+  });
+}
+
+// Connexion à MongoDB et démarrage du serveur
 mongoose.connect(process.env.MONGO_URL).then(() => {
   console.log("✅ MongoDB connected");
   app.listen(process.env.PORT, () =>
