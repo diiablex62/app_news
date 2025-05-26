5304dc7e90b1152d578c24a626a3e7aeb882047c
Aleexj <jalias59@outlook.fr>
2025-05-26 11:27:54 +0200

    Modification de l'URL de récupération des posts dans le composant NewsForm pour pointer vers l'API déployée sur Render.

--- front/src/components/NewsForm.jsx
+++ front/src/components/NewsForm.jsx
@@ -28,7 +28,7 @@ function NewsForm({ toggleForm }) {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:3000/api/posts", {
+      const response = await fetch("https://app-news-qfqs.onrender.com/api/posts", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
